870bd82a9d3d46f5e38254de6dfd3ab346b2819b
rosariodeveloper <rosariomassango@gmail.com>
2022-05-03 15:21:45 +0100

    Create product factory

---
+++ database/factories/ProductFactory.ts
@@ -0,0 +1,11 @@
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import Product from 'App/Models/Product'
+
+export const ProductFactory = Factory.define(Product, ({ faker }) => {
+  return {
+    category_id: 1,
+    name: faker.commerce.product(),
+    price: faker.commerce.price(),
+    description: faker.commerce.productDescription(),
+  }
+}).build()
