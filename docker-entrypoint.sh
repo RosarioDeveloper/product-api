9a66e394fd5fe8bae234dae9af7e488bc9d10235
rosariodeveloper <rosariomassango@gmail.com>
2022-05-07 22:36:24 +0100

    Fixing db:sedd  executtion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 [ ! -f /.env ] && cp dockerConfig/env.txt .env
 [ ! -f /.env ] && cp dockerConfig/env.txt .env.test
 yarn
-dumb-init node ace serve --watch --node-args="--inspect=0.0.0.0"
 node ace migration:run
 node ace db:seed
+dumb-init node ace serve --watch --node-args="--inspect=0.0.0.0"
 
 
