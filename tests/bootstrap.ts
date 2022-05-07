4a0680770fd97bd63b528a5e735b7b4e19796b6d
rosariodeveloper <rosariomassango@gmail.com>
2022-05-07 22:35:34 +0100

    Removing unsed codes

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -1,4 +1,3 @@
-import { Client } from 'pg'
 import { resolve } from 'path'
 import request from 'supertest'
 
@@ -8,20 +7,3 @@ require('dotenv').config({
 
 const HOST = `http://${process.env.HOST}:${process.env.PORT}`
 export const Http = request(HOST)
-
-export const DbConnection = (): Client => {
-  return new Client({
-    host: '172.17.0.1',
-    port: 5433,
-    user: 'postgres',
-    password: 'postgres',
-    //database: 'product_api_test',
-  })
-}
-
-export const DbInit = async (conn: Client) => {
-  try {
-    await conn.query(`DROP DATABASE IF EXISTS product_api_test`)
-    await conn.query(`CREATE DATABASE product_api_test`)
-  } catch (error) {}
-}
