e9c5c60e4fdde530f01ac07ca3d8a54acce1b079
rosariodeveloper <rosariomassango@gmail.com>
2022-05-07 19:49:58 +0100

    creating db when postgres contair is stated

--- dockerConfig/postgres-dev-init.sql
+++ dockerConfig/postgres-dev-init.sql
@@ -1,6 +1,1 @@
-DROP DATABASE IF EXISTS product_api;
 CREATE DATABASE product_api;
-
-
-/* CREATE USER rosario with encrypted password 'rosario';
-GRANT ALL PRIVILEGES ON DATABASE product_api TO rosario; */
